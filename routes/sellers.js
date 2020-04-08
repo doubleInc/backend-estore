cd982df70a1af75d6844940e643ece00cc11f4de
doubleInc <aalavi@protonmail.com>
2020-04-08 19:22:36 +1000

    Update Seller model and include address field-a

--- routes/sellers.js
+++ routes/sellers.js
@@ -5,10 +5,14 @@ const {
   getSeller,
   updateSeller,
   deleteSeller,
+  getSellerInLocale,
 } = require("../controllers/sellers");
 
 //routes
 router.route("/").get(getSellers).post(createSeller);
 router.route("/:id").get(getSeller).put(updateSeller).delete(deleteSeller);
+
+// find stores in locale
+router.route("/local/:postcode/:distance").get(getSellerInLocale);
 
 module.exports = router;
