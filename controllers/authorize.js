470a8fac4756890d08f0c30a84d2975ccc0b9830
doubleInc <aalavi@protonmail.com>
2020-04-12 11:23:40 +1000

    error handling added for some routes-a

--- controllers/authorize.js
+++ controllers/authorize.js
@@ -3,58 +3,80 @@ const User = require("../models/User");
 // register a user
 // ROUTE: POST /login/register
 exports.register = async (req, res, next) => {
-  const { name, email, password, image, address } = req.body;
+  try {
+    const { name, email, password, image, address } = req.body;
 
-  // create user
-  const user = await User.create({
-    name,
-    email,
-    password,
-    image,
-    address,
-  });
+    // create user
+    const user = await User.create({
+      name,
+      email,
+      password,
+      image,
+      address,
+    });
 
-  // create token
-  const token = user.getJWT();
+    // create token
+    const token = user.getJWT();
 
-  // respond with token
-  res.status(200).json({
-    success: true,
-    token,
-  });
+    // respond with token
+    res.status(200).json({
+      success: true,
+      token,
+    });
+  } catch (err) {
+    //
+    res.status(201).json({
+      success: false,
+      message: err,
+    });
+  }
 };
 
 // register a user
 // ROUTE: POST /login
 exports.login = async (req, res, next) => {
-  const { email, password } = req.body;
+  try {
+    const { email, password } = req.body;
 
-  // validate
-  if (!email || !password) {
-    return res.status(500);
+    // validate
+    if (!email || !password) {
+      return res.status(500);
+    }
+
+    // find a user
+    const user = await User.findOne({ email }).select("+password");
+
+    //if no user
+    if (!user) {
+      return res.status(401).json({
+        success: false,
+        message: "Authentication error, please check credentials.",
+      });
+    }
+
+    // check passwords match
+    const isMatch = await user.comparePassword(password);
+
+    if (!isMatch) {
+      return res.status(401).json({
+        success: false,
+        message: "Authentication error, please check credentials.",
+      });
+    }
+
+    // create token
+    const token = user.getJWT();
+
+    // respond with token
+    res.status(200).json({
+      success: true,
+      token,
+      userid: user._id,
+    });
+  } catch (err) {
+    res.status(201).json({
+      success: false,
+      message: err,
+    });
   }
-
-  // find a user
-  const user = await User.findOne({ email }).select("+password");
-
-  //if no user
-  if (!user) {
-    return res.status(401);
-  }
-
-  // check passwords match
-  const isMatch = await user.comparePassword(password);
-
-  if (!isMatch) {
-    return res.status(401);
-  }
-
-  // create token
-  const token = user.getJWT();
-
-  // respond with token
-  res.status(200).json({
-    success: true,
-    token,
-  });
 };
